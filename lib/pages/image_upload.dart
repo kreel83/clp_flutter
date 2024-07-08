4720b3abe62c3725c5cd003e91d67850b325c420
Kreel83 <marc.borgna@gmailcom>
2024-07-08 13:30:01 +0200

    fini

--- lib/pages/image_upload.dart
+++ lib/pages/image_upload.dart
@@ -34,36 +34,7 @@ class ImageUpload extends StatefulWidget {
 }
 
 class _ImageUploadState extends State<ImageUpload> {
-  // late Future<XFile?> _imageFile;
-
-  // late List<XFile>? imageFileList = [];
-  // final ImagePicker imagePicker = ImagePicker();
-  // List<bool> afficheCircles = [];
   bool afficheCirclesAll = false;
-
-  // Future<void> _pickMultiImage() async {
-  //   final List<XFile> selectedImages =
-  //       (await imagePicker.pickMultiImage()).cast<XFile>();
-  //   if (selectedImages.isNotEmpty) {
-  //     imageFileList!.addAll(selectedImages);
-  //   }
-  //   setState(() {
-  //     // ignore: unused_local_variable
-  //     for (XFile element in selectedImages) {
-  //       afficheCircles.add(false);
-  //     }
-  //   });
-  // }
-
-  // Future<void> _pickImage() async {
-  //   final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-
-  //   if (pickedFile != null) {
-  //     setState(() {
-  //       _imageFile = Future.value(XFile(pickedFile.path));
-  //     });
-  //   }
-  // }
 
   sendImageToAPI(List<XFile> imageFile, typeDepot, mission) async {
     var uri = Uri.parse(
@@ -161,91 +132,129 @@ class _ImageUploadState extends State<ImageUpload> {
                         ? const CenterMessageWidget(
                             texte:
                                 'Aucun document sélectionné\n cliquez sur + pour ajouter un document')
-                        : GridView.builder(
-                            itemCount: widget.imageFileList!.length,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    crossAxisSpacing: 8,
-                                    mainAxisSpacing: 8),
-                            itemBuilder: (BuildContext context, int index) {
-                              return GestureDetector(
-                                onLongPress: () {
-                                  showModalBottomSheet(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return SizedBox(
-                                        height: 200,
-                                        child: Column(
-                                          children: <Widget>[
-                                            ListTile(
-                                              leading: const Icon(Icons.share),
-                                              title: const Text('Télécharger'),
-                                              onTap: () async {
-                                                await sendImageToAPISolo(
-                                                    widget.imageFileList!,
-                                                    widget.typeDepot,
-                                                    widget.idMission,
-                                                    index);
+                        : Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: RichText(
+                                    text: const TextSpan(children: [
+                                  TextSpan(
+                                      text:
+                                          "- Appui long sur une vignette pour la retirer ou la télécharger.\n - appui sur ",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 14)),
+                                  WidgetSpan(
+                                    child: Icon(Icons.import_export,
+                                        color: Colors.black, size: 24),
+                                  ),
+                                  TextSpan(
+                                    text:
+                                        " (barre d'action) pour télécharger la totalité de la liste.",
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 14),
+                                  ),
+                                ])),
+                              ),
+                              const Divider(),
+                              Expanded(
+                                child: GridView.builder(
+                                    itemCount: widget.imageFileList!.length,
+                                    gridDelegate:
+                                        const SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 2,
+                                            crossAxisSpacing: 8,
+                                            mainAxisSpacing: 8),
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return GestureDetector(
+                                        onLongPress: () {
+                                          showModalBottomSheet(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return SizedBox(
+                                                height: 200,
+                                                child: Column(
+                                                  children: <Widget>[
+                                                    ListTile(
+                                                      leading: const Icon(
+                                                          Icons.share),
+                                                      title: const Text(
+                                                          'Télécharger'),
+                                                      onTap: () async {
+                                                        await sendImageToAPISolo(
+                                                            widget
+                                                                .imageFileList!,
+                                                            widget.typeDepot,
+                                                            widget.idMission,
+                                                            index);
 
-                                                Navigator.push(
-                                                  // ignore: use_build_context_synchronously
-                                                  context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) =>
-                                                          MissionPage(
-                                                              mission: widget
-                                                                  .idMission,
-                                                              collecte: widget
-                                                                  .idCollecte,
-                                                              defaultIndex: widget
-                                                                  .indexTab)),
-                                                );
-                                                Alert.showToast(
-                                                    "Document téléchargé avec succès");
-                                              },
+                                                        Navigator.push(
+                                                          // ignore: use_build_context_synchronously
+                                                          context,
+                                                          MaterialPageRoute(
+                                                              builder: (context) => MissionPage(
+                                                                  mission: widget
+                                                                      .idMission,
+                                                                  collecte: widget
+                                                                      .idCollecte,
+                                                                  defaultIndex:
+                                                                      widget
+                                                                          .indexTab)),
+                                                        );
+                                                        Alert.showToast(
+                                                            "Document téléchargé avec succès");
+                                                      },
+                                                    ),
+                                                    ListTile(
+                                                      leading: const Icon(
+                                                          Icons.delete),
+                                                      title: const Text(
+                                                          'Supprimer'),
+                                                      onTap: () {
+                                                        setState(() {
+                                                          widget.imageFileList!
+                                                              .removeAt(index);
+                                                          widget.afficheCircles
+                                                              .removeAt(index);
+                                                        });
+                                                        // Action pour supprimer
+                                                        Navigator.pop(context);
+                                                        Alert.showToast(
+                                                            "Image retirée avec succès");
+                                                      },
+                                                    ),
+                                                  ],
+                                                ),
+                                              );
+                                            },
+                                          );
+                                        },
+                                        child: Stack(
+                                          alignment: Alignment.center,
+                                          children: [
+                                            Opacity(
+                                              opacity: (!widget.afficheCircles[
+                                                          index] &&
+                                                      afficheCirclesAll)
+                                                  ? 0.5
+                                                  : 1,
+                                              child: Image.file(
+                                                File(widget
+                                                    .imageFileList![index]
+                                                    .path),
+                                                fit: BoxFit.cover,
+                                              ),
                                             ),
-                                            ListTile(
-                                              leading: const Icon(Icons.delete),
-                                              title: const Text('Supprimer'),
-                                              onTap: () {
-                                                setState(() {
-                                                  widget.imageFileList!
-                                                      .removeAt(index);
-                                                  widget.afficheCircles
-                                                      .removeAt(index);
-                                                });
-                                                // Action pour supprimer
-                                                Navigator.pop(context);
-                                                Alert.showToast(
-                                                    "Image retirée avec succès");
-                                              },
-                                            ),
+                                            if (widget.afficheCircles[index])
+                                              const CircularProgressIndicator()
                                           ],
                                         ),
                                       );
-                                    },
-                                  );
-                                },
-                                child: Stack(
-                                  alignment: Alignment.center,
-                                  children: [
-                                    Opacity(
-                                      opacity: (!widget.afficheCircles[index] &&
-                                              afficheCirclesAll)
-                                          ? 0.5
-                                          : 1,
-                                      child: Image.file(
-                                        File(widget.imageFileList![index].path),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                    if (widget.afficheCircles[index])
-                                      const CircularProgressIndicator()
-                                  ],
-                                ),
-                              );
-                            })),
+                                    }),
+                              ),
+                            ],
+                          )),
               ),
             ],
           ),
